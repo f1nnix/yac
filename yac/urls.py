7b3f9568dcc28d635158bf759b7d1e244fe5ba32
Ilya Rusanen <ilya@rusanen.co.uk>
2019-01-13 23:19:32 +0300

    [feat] +custom User model

--- yac/urls.py
+++ yac/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path  # For django versions from 2.0 and up
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('manage/', admin.site.urls),
+    path('auth/', include('authtools.urls')),
 ]
